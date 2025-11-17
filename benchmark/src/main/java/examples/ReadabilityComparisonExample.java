59c70d1d569c9f8ebdda0f263902cbb1cf7ba58e
Stefan Boudri <stefan.boudri@hva.nl>
2025-11-17 23:25:43 +0100

    added example

--- benchmark/src/main/java/examples/ReadabilityComparisonExample.java
+++ benchmark/src/main/java/examples/ReadabilityComparisonExample.java
@@ -1,7 +1,6 @@
 package examples;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class ReadabilityComparisonExample {
--- benchmark/src/main/java/examples/WhenToAvoidStreamsExample.java
+++ benchmark/src/main/java/examples/WhenToAvoidStreamsExample.java
@@ -1,14 +1,14 @@
 package examples;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class WhenToAvoidStreamsExample {
 
     public static void main(String[] args) {
 
-        System.out.println("Indexed loop example (good for sequential logic):");
+        System.out.println("Indexed loop example:");
         List<Integer> numbers = List.of(10, 20, 30, 40, 50);
 
         for (int i = 0; i < numbers.size() - 1; i++) {
@@ -17,11 +17,28 @@ public class WhenToAvoidStreamsExample {
             System.out.println(current + " -> " + next);
         }
 
-        List<Integer> unsafeList = new ArrayList<>();
+        System.out.println("\nUnsafe parallel stream example 1:");
+        List<Integer> numbersToUnsafeList = new ArrayList<>();
 
         numbers.parallelStream()
+                .forEach(numbersToUnsafeList::add);
+
+        System.out.println("Numbers are now likely unordered:" + numbersToUnsafeList);
+
+        System.out.println("\nUnsafe parallel stream example 2:");
+        List<Integer> unsafeList = new ArrayList<>();
+
+        IntStream.range(0, 100_000)
+                .parallel()
                 .forEach(unsafeList::add);
 
-        System.out.println("Result of unsafeList" + unsafeList);
+        System.out.println("Expected size: 100000");
+        System.out.println("Actual size:   " + unsafeList.size());
+
+        // Check for duplicates or missing values
+        long uniqueElementsCount = unsafeList.stream().distinct().count();
+
+        System.out.println("Unique elements: " + uniqueElementsCount);
+        System.out.println("Amount of duplicates:" + (unsafeList.size() - uniqueElementsCount));
     }
 }
